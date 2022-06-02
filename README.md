c68a9c9b9a985e210c0a6acc3b491214958ba49d
andres97473 <andres97473@hotmail.com>
2022-06-01 20:51:17 -0500

    Primer commit - REST server

---
+++ README.md
@@ -0,0 +1,20 @@
+# Socket-Server
+
+Reconstruir modulos de Node
+
+```
+npm install
+```
+
+Generar el dist
+
+```
+tsc -w
+```
+
+Levantar servidor
+
+```
+node dist/
+nodemon dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+router.get("/mensajes", (req: Request, res: Response) => {
+  res.json({
+    ok: true,
+    mensaje: "GET - Listo",
+  });
+});
+
+router.post("/mensajes", (req: Request, res: Response) => {
+  const { cuerpo, de } = req.body;
+
+  res.json({
+    ok: true,
+    cuerpo,
+    de,
+  });
+});
+router.post("/mensajes/:id", (req: Request, res: Response) => {
+  const { cuerpo, de } = req.body;
+  const id = req.params.id;
+
+  res.json({
+    ok: true,
+    cuerpo,
+    de,
+    id,
+  });
+});
+
+export default router;
