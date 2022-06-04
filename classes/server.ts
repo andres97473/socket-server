a43cf9d285e3d075fb37aca0f4822dd72810cad2
andres97473 <andres97473@hotmail.com>
2022-06-04 18:58:04 -0500

    Seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,54 @@
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import { Server as socketIO } from "socket.io";
+import http from "http";
+
+import * as socket from "../sockets/socket";
 
 export default class Server {
+  private static _instance: Server;
+
   public app: express.Application;
   public port: number;
 
-  constructor() {
+  public io: socketIO;
+  private httpServer: http.Server;
+
+  private constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+
+    this.httpServer = new http.Server(this.app);
+
+    this.io = new socketIO(this.httpServer, {
+      cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    this.escucharSockets();
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private escucharSockets() {
+    console.log("Escuchando conexiones - sockets");
+
+    this.io.on("connection", (cliente) => {
+      console.log("Cliente conectado");
+
+      // Mensajes
+      socket.mensaje(cliente, this.io);
+
+      // Desconectar
+      socket.desconectar(cliente);
+    });
   }
 
   start(callback: Function) {
-    this.app.listen(this.port, callback());
+    this.httpServer.listen(this.port, callback());
   }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import router from "./routes/router";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser - convertir lo que me enviar en el body en un objeto js
 server.app.use(bodyParser.urlencoded({ extended: true }));
